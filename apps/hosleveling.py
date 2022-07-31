46dcf38852d9576352ced3c4827d4af2a23ba313
Administrator <littlelumos@foxmail.com>
2022-07-31 19:52:11 +0800

    new hosleveling coding

---
+++ apps/hosleveling.py
@@ -0,0 +1,47 @@
+import win32api
+import win32gui
+
+import apps.library.winunder as wu
+import apps.library.wincom as wc
+
+handle = wc.getHandle("《风暴英雄》")
+time_las = 0
+
+
+def onClose():
+    wc.showWindow(handle)
+
+
+def getActualPos(x, y):
+    width, height = wc.getWindowSize(handle)
+    x = x * (width / 2560)
+    y = y * (height / 1440)
+    return int(x), int(y)
+
+
+def antiAfk():
+    retail = win32gui.GetForegroundWindow()
+
+    # 确定点击窗口位置
+    x, y = wc.getWindowPos(handle)
+    x1, y1 = getActualPos(1050, 835)
+    x2, y2 = getActualPos(1090, 1330)
+    x3, y3 = getActualPos(70, 1380)
+    # 点击窗口
+    wu.fake_active(handle, True)
+    wu.click(handle, x1, y1)  # 游戏中途防掉线
+    wu.click(handle, x2, y2)  # 游戏结束后进行排队
+    wu.click(handle, x3, y3)  # 游戏结算返回主界面
+    wu.fake_active(retail, False)
+
+
+def main():
+    afk_timer = wc.sec_timer(20, antiAfk)
+    win32api.SetConsoleCtrlHandler(onClose(), True)
+    while True:
+        afk_timer.run()
+
+
+main()
+
+# pyinstaller.exe -F E:\PyAutoPlayer\apps\hosleveling.py
---
+++ main.py
@@ -0,0 +1,6 @@
+from apps.hosleveling import main
+
+if __name__ == '__main__':
+    main()
+
+# pyinstaller.exe -F E:\PyAutoPlayer\main.py
