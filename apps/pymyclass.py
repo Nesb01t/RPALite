798b321442cc1912891525e964e2360dd655cac0
Administrator <littlelumos@foxmail.com>
2022-09-05 19:10:24 +0800

    add:dict轮询

--- apps/pymyclass.py
+++ apps/pymyclass.py
@@ -9,16 +9,29 @@ driver = webdriver.Firefox(service=s)
 driver.get("https://newjwxt.zust.edu.cn/jwglxt/xsxk/zzxkyzb_cxZzxkYzbIndex.html?gnmkdm=N253512&layout=default&su"
            "=1210204126")
 
+# create tr_dict
+tr_dict = {
+    "凌紫乔5 周四3-4": "tr_E05E9C4438BFD453E053640C10AC291A",
+    "凌紫乔6 周四6-7": "tr_E05E9C4438C3D453E053640C10AC291A",
+    "凌紫乔7 周四8-9": "tr_E05D3EA5A37AAF21E053640C10AC7321"
+}
 
+
+# tr_id对应的选课是否已满
 def isFull(tr_id):
     item = driver.find_element("id", tr_id)
     full = item.find_element("class name", "full")
     return full.text == "已满"
 
 
+input()  # 输入任意字符执行
 while True:
-    input()
-    if isFull("tr_E05EFFAC58C8DDD6E053640C10ACADDD"):
-        print("已经满了")
-    else:
-        print("选课还没满")
+    time.sleep(2)  # 查询间隔
+    button_request = driver.find_element("name", "query")
+    if button_request:
+        button_request.click()
+        for name in tr_dict:
+            if tr_dict[name]:
+                print(name + "是否可选: TrueTrueTrueTrueTrueTrueTrueTrue")
+            else:
+                print(name + "是否可选: false")
